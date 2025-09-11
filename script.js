6b344ee81dd41ddddc3d72727adc6781f6e2e856
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-09-11 19:13:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 function createTable() {
     //Write your code here
+     let rn = prompt("Input number of rows");
+  let cn = prompt("Input number of columns");
+
+  
+  rn = parseInt(rn);
+  cn = parseInt(cn);
+
+  
+  if (isNaN(rn) || isNaN(cn)) {
+    alert("Please enter valid numeric values.");
+    return;
+  }
+  if (rn <= 0 || cn <= 0) {
+    alert("Number of rows and columns must be greater than 0.");
+    return;
+  }
+
+  
+  const table = document.getElementById("myTable");
+
+  
+  table.innerHTML = "";
+
+  
+  for (let i = 0; i < rn; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < cn; j++) {
+      const cell = document.createElement("td");
+      cell.textContent = `Row-${i} Column-${j}`;
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
   
 }
